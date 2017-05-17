a9bb076f10ae860cb47795601fb5b1566063c558
bitchiko tchelidze <bubachelidze1@gmail.com>
2017-05-17 12:45:34 +0400

    Updated to latest version of .NET Core.
    Added XML declaration to generated xml.

--- src/WilderMinds.RssSyndication/Feed.cs
+++ src/WilderMinds.RssSyndication/Feed.cs
@@ -1,49 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace WilderMinds.RssSyndication
 {
-  public class Feed
-  {
-    public string Description { get; set; }
-    public Uri Link { get; set; }
-    public string Title { get; set; }
-    public string Copyright { get; set; }
-
-    public ICollection<Item> Items { get; set; } = new List<Item>();
-
-    public string Serialize()
+    public class Feed
     {
-      var doc = new XDocument(new XElement("rss"));
-      doc.Root.Add(new XAttribute("version", "2.0"));
+        public string Description { get; set; }
+        public Uri Link { get; set; }
+        public string Title { get; set; }
+        public string Copyright { get; set; }
 
-      var channel = new XElement("channel");
-      channel.Add(new XElement("title", this.Title));
-      channel.Add(new XElement("link", this.Link.AbsoluteUri));
-      channel.Add(new XElement("description", this.Description));
-      channel.Add(new XElement("copyright", this.Copyright));
-      doc.Root.Add(channel);
+        public ICollection<Item> Items { get; set; } = new List<Item>();
 
-      foreach (var item in Items)
-      {
-        var itemElement = new XElement("item");
-        itemElement.Add(new XElement("title", item.Title));
-        itemElement.Add(new XElement("link", item.Link.AbsoluteUri));
-        itemElement.Add(new XElement("description", item.Body));
-        if (item.Author != null) itemElement.Add(new XElement("author", $"{item.Author.Email} ({item.Author.Name})"));
-        foreach (var c in item.Categories) itemElement.Add(new XElement("category", c));
-        if (item.Comments != null) itemElement.Add(new XElement("comments", item.Comments.AbsoluteUri));
-        if (!string.IsNullOrWhiteSpace(item.Permalink)) itemElement.Add(new XElement("guid", item.Permalink));
-        var dateFmt = item.PublishDate.ToString("r");
-        if (item.PublishDate != DateTime.MinValue) itemElement.Add(new XElement("pubDate", dateFmt));
-        channel.Add(itemElement);
-      }
+        public string Serialize()
+        {
+            var doc = new XDocument(new XElement("rss"));
+            doc.Root.Add(new XAttribute("version", "2.0"));
 
-      return doc.ToString();
+            var channel = new XElement("channel");
+            channel.Add(new XElement("title", Title));
+            channel.Add(new XElement("link", Link.AbsoluteUri));
+            channel.Add(new XElement("description", Description));
+            channel.Add(new XElement("copyright", Copyright));
+            doc.Root.Add(channel);
 
+            foreach (var item in Items)
+            {
+                var itemElement = new XElement("item");
+                itemElement.Add(new XElement("title", item.Title));
+                itemElement.Add(new XElement("link", item.Link.AbsoluteUri));
+                itemElement.Add(new XElement("description", item.Body));
+                if (item.Author != null)
+                    itemElement.Add(new XElement("author", $"{item.Author.Email} ({item.Author.Name})"));
+                foreach (var c in item.Categories) itemElement.Add(new XElement("category", c));
+                if (item.Comments != null) itemElement.Add(new XElement("comments", item.Comments.AbsoluteUri));
+                if (!string.IsNullOrWhiteSpace(item.Permalink)) itemElement.Add(new XElement("guid", item.Permalink));
+                var dateFmt = item.PublishDate.ToString("r");
+                if (item.PublishDate != DateTime.MinValue) itemElement.Add(new XElement("pubDate", dateFmt));
+                channel.Add(itemElement);
+            }
+
+            return doc.ToStringWithDeclaration();
+        }
     }
-  }
 }
