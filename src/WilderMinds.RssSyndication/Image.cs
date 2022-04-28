26324f82730629e7b22237daf49370a185fb66ad
jonms90 <s231866@gmail.com>
2022-04-28 10:58:12 +0200

    Image xml summaries and guard clauses

--- src/WilderMinds.RssSyndication/Image.cs
+++ src/WilderMinds.RssSyndication/Image.cs
@@ -8,13 +8,22 @@ namespace WilderMinds.RssSyndication
     {
         public Image(Uri url, string title, Uri link)
         {
-            Url = url;
-            Title = title;
-            Link = link;
+            Url = url ?? throw new ArgumentNullException(nameof(url));
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Link = link ?? throw new ArgumentNullException(nameof(link));
         }
 
+        /// <summary>The URL of a GIF, JPEG or PNG image that represents the channel.</summary>
         public Uri Url { get; }
+
+        /// <summary>
+        /// Image description used in ALT attribute of the HTML image tag when the channel is rendered in HTML.
+        /// </summary>
         public string Title { get; }
+
+        /// <summary>
+        /// The URL of the site. When the channel is rendered, the image is a link to the site.
+        /// </summary>
         public Uri Link { get; }
     }
 }
